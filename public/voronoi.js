5829b7aeb69a5f34fcd9fe0ed6ccd463b916beb7
Andre Kolell <andre.kolell@bluesummit.de>
2012-02-25 21:26:04 +0100

    Minimum Distance between Points.

--- public/voronoi.js
+++ public/voronoi.js
@@ -1,9 +1,21 @@
-var w = 960,
-    h = 500;
+var w = 960, h = 500;
+var IMAGES_BEING_DISPLAYED = 20;
+var MIN_DISTANCE = 100;
 
-var vertices = d3.range(100).map(function(d) {
-  return [Math.random() * w, Math.random() * h];
-});
+//var vertices = d3.range(30).map(function(d) {
+//  return [Math.random() * w, Math.random() * h];
+//});
+
+var vertices = [];
+for(var i = 0; i < IMAGES_BEING_DISPLAYED; i++) {
+	new_point = [Math.random() * w, Math.random() * h];
+	var min_dist = 1000;
+	for(var i in vertices) {
+		var distance = Math.sqrt((new_point[0] - vertices[i][0]) * (new_point[0] - vertices[i][0]) + (new_point[1] - vertices[i][1]) * (new_point[1] - vertices[i][1]));
+		if(!min_dist || distance < min_dist) min_dist = distance;
+	}
+	if(!min_dist || min_dist > MIN_DISTANCE) vertices.push(new_point);
+}
 
 var svg = d3.select("#chart")
   .append("svg")
