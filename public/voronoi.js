70e1a8e1f6f42ff8b171998805436b4b36af39dc
Jerome Morin-Drouin <jmdrouin@gmail.com>
2012-02-25 21:51:46 +0100

    Show photo with clip path

--- public/voronoi.js
+++ public/voronoi.js
@@ -1,34 +1,33 @@
-var w = 960,
+var w = 1000,
     h = 500;
 
-var vertices = d3.range(100).map(function(d) {
+var vertices = d3.range(6).map(function(d) {
   return [Math.random() * w, Math.random() * h];
 });
 
 var svg = d3.select("#chart")
   .append("svg")
     .attr("width", w)
+    .attr("id", "root")
     .attr("height", h)
     .attr("class", "PiYG")
     .on("mousemove", update);
 
+
 svg.selectAll("path")
     .data(d3.geom.voronoi(vertices))
-  .enter().append("path")
-    .attr("class", function(d, i) { return i ? "q" + (i % 9) + "-9" : null; })
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
+  .enter().append("clipPath")
+    .attr("id", function(d, i) { return i ? "id" + i : null; })
+    .append("path")
+      .attr("class", function(d, i) { return i ? "q" + (i % 9) + "-9" : null; })
+      .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
 
-svg.selectAll("circle")
-    .data(vertices.slice(1))
-  .enter().append("circle")
-    .attr("transform", function(d) { return "translate(" + d + ")"; })
-    .attr("r", 2);
+var count = 0;
+$('.photo')
+  .attr({height:500, width:1000})
+  .attr("clip-path", function(d,i) {return "url(#id"+ (count++) +")" })
+  .prependTo('#root');
+
 
 function update() {
-  vertices[0] = d3.mouse(this);
-  svg.selectAll("path")
-      .data(d3.geom.voronoi(vertices)
-      .map(function(d) { return "M" + d.join("L") + "Z"; }))
-      .filter(function(d) { return this.getAttribute("d") != d; })
-      .attr("d", function(d) { return d; });
 }
