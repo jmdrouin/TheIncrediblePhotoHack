86efe2d38becdfee83e8538bbc7b18667d8cdb33
André Rieck <4ndr3r13ck@googlemail.com>
2012-03-05 10:10:17 +0100

    dont send empty search requests

--- public/application.js
+++ public/application.js
@@ -1,19 +1,22 @@
 $(function() {
   $('#album_search').bindWithDelay('keyup',function() {
-    $.ajax({
-      url: '/search/eyeem/albums',
-      data: {q: $(this).val()},
-      success: function(data) {
-        var list = $('#albums_list'),
-          albums = data['albums']['items'],
-          list_item;
-        list.empty();
-        for (var i = albums.length - 1; i >= 0; i--) {
-          list_item = $(document.createElement("li"));
-          $(document.createElement("a")).attr({ href: '/eyeem/' + albums[i].id }).text(albums[i].name).appendTo(list_item);
-          list.append(list_item);          
-        };
-      }
-    })
+    var query = $(this).val().trim();
+    if (query != '') {
+      $.ajax({
+        url: '/search/eyeem/albums',
+        data: {q: query},
+        success: function(data) {
+          var list = $('#albums_list'),
+            albums = data['albums']['items'],
+            list_item;
+          list.empty();
+          for (var i = albums.length - 1; i >= 0; i--) {
+            list_item = $(document.createElement("li"));
+            $(document.createElement("a")).attr({ href: '/eyeem/' + albums[i].id }).text(albums[i].name).appendTo(list_item);
+            list.append(list_item);          
+          };
+        }
+      })
+    }
   },1000,true)
 })
