d8f37b22201b8d15fb3ea544f0e713a4d95c6009
André Rieck <4ndr3r13ck@googlemail.com>
2012-06-24 20:03:59 +0200

    changed to EyeEmConnector-Gem

--- Gemfile
+++ Gemfile
@@ -2,7 +2,5 @@ source :rubygems
 gem 'sinatra', '1.3.2'
 gem 'sinatra-contrib', '1.3.1'
 gem 'thin', '1.3.1'
-gem 'oauth2'
 gem 'therubyracer'
-gem 'json'
-gem 'faraday'
+gem 'EyeEmConnector'
--- photo_hack.rb
+++ photo_hack.rb
@@ -3,15 +3,14 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 #require 'sinatra/config_file'
 require 'v8'
-#require 'faraday'
-require './eye_em_connector.rb'
+require 'EyeEmConnector'
 
-#config_file 'config/services.yml'
-#conn = Faraday.new(:url => 'https://www.eyeem.com/api/v2/', :params => {:client_id => settings.eyeem.client_id}
+EyeEmConnector.configure do |config|
+  config.client_id = ENV['EYEEM_CLIENT_ID']
+end
 
 get '/' do
-  eyeem = EyeEmConnector.new(ENV['EYEEM_CLIENT_ID'])
-  @recommendations = eyeem.albums_recommended(:limit => 5)
+  @recommendations = EyeEmConnector.albums_recommended(:limit => 5)
   erb :home
 end
 
@@ -26,14 +25,12 @@ end
 
 get '/photos/eyeem/:album_id' do 
   content_type :json
-  eyeem = EyeEmConnector.new(ENV['EYEEM_CLIENT_ID'])
-  eyeem.album_photos(params[:album_id]).to_json
+  eyeem = EyeEmConnector.album_photos(params[:album_id]).to_json
 end
 
 get '/search/eyeem/albums' do 
   content_type :json
-  eyeem = EyeEmConnector.new(ENV['EYEEM_CLIENT_ID'])
-  eyeem.albums(:q => params[:q], :limit => 5, :minPhotos => 10).to_json
+  eyeem = EyeEmConnector.albums(:q => params[:q], :limit => 5, :minPhotos => 10).to_json
 end
 
 get '/about' do
